9ec911beafe16c70be940ea712b1dba631de0f3e
Alipacman <ali_pourasad@yahoo.de>
2018-11-08 22:04:08 +0100

    -add Edge class -update graph& vertex class

--- No4/Graph.py
+++ No4/Graph.py
@@ -1,9 +1,16 @@
+from .Vertex import Vertex
+from .Edge import Edge
+
+
+
+
 class Graph:
 
     edges = []
     vertexes = []
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, start_x_pos, start_y_pos):
+        start_vertex = Vertex(start_x_pos, start_y_pos)
+        self.vertexes.append(start_vertex)
+
 
--- No4/Vertex.py
+++ No4/Vertex.py
@@ -1,6 +1,19 @@
+import enum
+
+class Type(enum.Enum):
+    unvisited = 'unvisited'
+    visited = 'visited'
+    blocked = 'blocked'
+    start = 'start'
+    goal = 'goal'
+    portal = 'portal'
+
 class Vertex:
 
-    def __init__(self, x_pos, y_pos):
+    def __init__(self, x_pos, y_pos, type):
         self.x = x_pos
         self.y = y_pos
+        if type not in (Type.unvisited, Type.visited, Type.blocked, Type.start, Type.goal, Type.portal):
+             raise ValueError('type not valid')
+        self.type = type
 
