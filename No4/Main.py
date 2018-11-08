716b95d285b1b3702b083a23d1aa82881936a2fb
Alipacman <ali_pourasad@yahoo.de>
2018-11-08 22:06:16 +0100

    -add main

---
+++ No4/Main.py
@@ -0,0 +1,28 @@
+#TODO: convert this to: txt_into_Graph
+
+#Methods for string array switch
+def gamefield_into_array(height, width):
+
+    gamefield_array = np.empty(shape=[height, width], dtype ="str")
+
+    for y in range(height):
+        for x in range(width):
+            gamefield_array[y][x] = gamefield[((y * width + 1 * y) + x)]
+
+    return gamefield_array
+
+def array_into_gamefield(array):
+
+    gamfield_str = ""
+
+    for y in range(len(array)):
+        if y != 0:
+            gamfield_str = gamfield_str + "\n"
+        for x in range(len(array[0])):
+            gamfield_str = gamfield_str + str(array[y][x])
+
+    return gamfield_str
+
+
+#gameFields
+gamefield = open('blatt3_environment.txt', 'r').read()
